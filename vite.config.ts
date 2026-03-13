5f7711c04be277b7c7705b72b3a69c760874cc16
Emad Jumaah <emadjumaah@gmail.com>
2026-03-13 08:59:04 +0300

    v3.3.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: /^@intenttext\/core$/,
-        replacement: fileURLToPath(
-          new URL("./src/core-compat.ts", import.meta.url),
-        ),
-      },
-    ],
-  },
   build: {
     rollupOptions: {
       output: {
